9f4c3a8c0129a9c44b89b37cd54bb8801a5a48a3
abdula <abdulwahabaldahhak@gmail.com>
2025-08-16 13:06:34 +0300

    theje

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,11 +31,14 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(325, 316);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(185, 24);
             this.textBox1.TabIndex = 0;
@@ -53,7 +56,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(395, 121);
+            this.button1.Location = new System.Drawing.Point(325, 159);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(178, 98);
             this.button1.TabIndex = 2;
@@ -61,11 +64,32 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Century", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(129, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 49);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "الضحاك";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Century", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(107, 298);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 70);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ابراهيم ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -81,6 +105,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -39,5 +39,10 @@ namespace WindowsFormTemProject
         {
             //add buton
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
