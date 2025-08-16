d1f7b0699b99bd33d2a879ced3dbf50ba84b9f7c
abdula <abdulwahabaldahhak@gmail.com>
2025-08-16 13:12:06 +0300

    change buttn back coler tgo balc

--- Form1.cs
+++ Form1.cs
@@ -40,5 +40,10 @@ namespace WindowsFormTemProject
             //add buton
             // add
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Black;
+        }
     }
 }
